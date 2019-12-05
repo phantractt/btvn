f1ddff665083cb6d30ac8814cfcb9cf8189f7dbc
unknown <phantractt@gmail.com>
2019-12-05 15:03:59 +0700

    btvn

--- BTVN6/src/btvn6/Complex.java
+++ BTVN6/src/btvn6/Complex.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package btvn6;
 
 /**
@@ -10,5 +6,55 @@ package btvn6;
  * @author ASUS
  */
 public class Complex {
-    
+
+    String loai = "SOTHUC";
+    String ten;
+    int phanthuc;
+    int phanao;
+
+    public Complex() {
+        ten = "chuadatten";
+        phanthuc = 1;
+        phanao = 1;
+    }
+
+    public Complex(int a) {
+        this.phanthuc = a;
+    }
+
+    public Complex(int phanthuc, int phanao) {
+
+        this.phanthuc = phanthuc;
+        this.phanao = phanao;
+
+    }
+     Complex cong(Complex sophuc) {
+        Complex ketqua = new Complex();
+        ketqua.phanthuc = this.phanthuc + sophuc.phanthuc;
+        ketqua.phanao = this.phanao + sophuc.phanao;
+        return ketqua;
+    }
+         Complex tru(Complex sophuc) {
+        Complex ketqua = new Complex();
+        ketqua.phanthuc = this.phanthuc - sophuc.phanthuc;
+        ketqua.phanao = this.phanao - sophuc.phanao;
+        return ketqua;
+    }
+         Complex nhan(Complex sophuc) {
+        Complex ketqua = new Complex();
+        ketqua.phanthuc = this.phanthuc * sophuc.phanthuc - this.phanao * sophuc.phanao;
+        ketqua.phanao = this.phanthuc * sophuc.phanao + this.phanao * sophuc.phanthuc;
+        return ketqua;
+    }
+          Complex chia(Complex sophuc) {
+        Complex ketqua = new Complex();
+        ketqua.phanthuc = (sophuc.phanthuc * this.phanthuc + this.phanao * sophuc.phanao) / (this.phanthuc ^ 2 + this.phanao ^ 2);
+        ketqua.phanao = (this.phanthuc * sophuc.phanao - sophuc.phanthuc * this.phanao) / (this.phanthuc ^ 2 + this.phanao ^ 2);
+        return ketqua;
+    }
+
+    void shows() {
+        System.out.println(this.ten + " : " + this.phanthuc +" + "+ this.phanao + "*i");
+    }
+
 }
